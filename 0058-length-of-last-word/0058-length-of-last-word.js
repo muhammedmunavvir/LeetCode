682e9f768612454ad2305dc18f55a60cd8aa0122
muhammedmunavvir <munavvirmuhammed296@gmail.com>
2024-08-20 10:46:25 +0530

    Time: 45 ms (86.81%), Space: 49.1 MB (17.58%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.js
+++ 0058-length-of-last-word/0058-length-of-last-word.js
@@ -2,27 +2,9 @@
  * @param {string} s
  * @return {number}
  */
- var arr=[]
+ 
 var lengthOfLastWord = function(s) {
-//   var arr=s.split(" ")
-//   var length=arr[arr.length-1].length
 
-//   return length 
-// let count=0; 
-// let flag=false;
-//   for(var i=s.length-1;i>=0;i--){
-//     if(flag==true && s.charAt(i)==' '){
-//         return count;
-//     }
-//     if(s.charAt(i)==' '){
-//         continue;
-//     }
-//     else {
-//         count++;
-//         flag=true;
-//     }
-//   }
-//   return count;
 var arr=s.split(' ');
 for(var i=arr.length-1;i>=0;i--){
     if(arr[i].length==0){
@@ -30,7 +12,7 @@ for(var i=arr.length-1;i>=0;i--){
     }
     else{
         return arr[i].length;
-    }
+    } 
 } 
-return 0;
+
 };
