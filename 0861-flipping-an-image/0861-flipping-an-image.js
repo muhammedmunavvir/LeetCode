ecc9bf801632739effb7f9ffb80c1e4d7c06e387
muhammedmunavvir <munavvirmuhammed296@gmail.com>
2024-09-28 10:30:54 +0530

    Time: 69 ms (12.48%), Space: 52.3 MB (10.6%) - LeetHub

--- 0861-flipping-an-image/0861-flipping-an-image.js
+++ 0861-flipping-an-image/0861-flipping-an-image.js
@@ -3,18 +3,16 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(image) {
-   for (let i = 0; i < image.length; i++) {
-        // Step 1: Reverse the row
-        image[i].reverse();
-
-        // Step 2: Invert the values in the row
-        for (let j = 0; j < image[i].length; j++) {
-            if (image[i][j] === 0) {
-                image[i][j] = 1; // Change 0 to 1
-            } else {
-                image[i][j] = 0; // Change 1 to 0
-            }
+   
+   for(let i=0;i<image.length;i++){
+      image[i].reverse()
+      for(let j=0;j<image.length;j++){
+        if(image[i][j]===0){
+            image[i][j]=1
+        }else{
+            image[i][j]=0
         }
-    }
-    return image;
+      }
+   }
+   return image;
 };
